5059fc77a058c03aad9416335f7f4244427a8ca7
medic code <u40as7@gmail.com>
2021-09-14 08:30:14 +0100

    new style changes implemented

--- Components/carousel.js
+++ Components/carousel.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {View, Text, Dimensions} from 'react-native';
 
-export const SLIDER_WIDTH = Dimensions.get('window').width - 200;
-export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.9);
+export const SLIDER_WIDTH = Dimensions.get('window').width - 100;
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 1.2);
 
 const CarouselCardItem = ({item,index}) => {
   
@@ -10,14 +10,14 @@ const CarouselCardItem = ({item,index}) => {
    
     <View key={index} style={{flex:1,
       backgroundColor:'white',
-alignItems:'center',justifyContent: 'space-between',
-borderRightWidth: 2,
-    
+      alignItems:'center',justifyContent: 'space-between',
+      backgroundColor:'rgba(22,122,121,0.3)', 
+      borderRadius: '5',
       height: 80,
       padding: 10,
       margin:10 }}>
-      <Text style={{fontSize: 13, fontWeight: "600", alignSelf: 'center', marginBottom: 10}} >{item[0]}</Text>
-      {<Text style={{fontSize: 16, fontWeight: "600", alignSelf: 'center' }} >{item[1]}</Text>
+      <Text style={{fontSize: 13, fontWeight: "600", alignSelf: 'center', marginBottom: 10, color: 'rgb(54,69,79)'}} >{item[0]}</Text>
+      {<Text style={{fontSize: 16, fontWeight: "600", alignSelf: 'center', color: 'rgb(54,69,79)' }} >{item[1]}</Text>
     }
       
     </View>
--- Components/iol.js
+++ Components/iol.js
@@ -10,8 +10,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 10,
-    backgroundColor: 'white',
+
+    backgroundColor: 'white'
 
   },
   paragraph: {
@@ -24,6 +24,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     flex: 1,
     flexWrap: 'wrap',
+    color: '#1C4A7A',
+    opacity: 1
   }
 
 });
@@ -53,35 +55,36 @@ const Iol = ({route,navigation, }) => {
 return (
   <ScrollView>
   <View style={styles.container}> 
-    <Button
+    {/* <Button
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
-      />
-  <View style={{flex:0.7, flexDirection: 'row',flexWrap: 'wrap' }}>
-    <Icon name={'reader'} color={'blue'} size={60}/>
+      /> */}
+  <View style={{flex:1, padding:10, flexDirection: 'row',flexWrap: 'wrap', backgroundColor:'rgba(99, 142, 187, 0.5)' }}>
+    <Icon name={'reader'} color={'blue'} size={40}/>
     <View style={{flex:1, flexDirection: 'column', marginLeft: 30, }}>
     <Title1 style={styles.title}>{data.lens}</Title1> 
     
     </View>
    
   </View>
-  <View style={styles.container}>
-      <Card>
-      <Title1 style={{marginLeft: 5,marginBottom: 10,color: iOSColors.gray }}>Product Information</Title1>
-      <Text style={{fontSize: 16, marginLeft: 10}}><Text style={{fontWeight:'bold'}}>Name:</Text> {data.lens}</Text>
-      <Text style={{fontSize: 16, marginLeft: 10}}><Text style={{fontWeight:'bold'}}>Haptic Material:</Text> {data.hapmaterial}</Text>
-      <Text style={{fontSize: 16, marginLeft: 10}}><Text style={{fontWeight:'bold'}}>Optic Material: </Text>{data.opmaterial}</Text>
-      <Text style={{fontSize: 16, marginLeft: 10,fontWeight:'bold'}}>Dimensions:</Text>
-      <Text style={{fontSize: 16, marginLeft: 50}}>HAPTIC: {data.haptic} </Text>
-      <Text style={{fontSize: 16,marginLeft: 50,marginBottom: 10}}>OPTIC:  {data.opticdia} </Text>
-      </Card>
+  <View style={{flex: 1,
+    justifyContent: 'center',
+    padding: 10, backgroundColor: 'rgba(99, 142, 187, 0.2)'}}>
+      <Title1 style={{marginLeft: 5,marginBottom: 10,color: '#1C4A7A', fontSize: 17, fontWeight: '600'}}>Product Information</Title1>
+      <Text style={{fontSize: 16, marginLeft: 10, color: 'rgb(54,69,79)'}}><Text style={{fontWeight:'bold'}}>Name:</Text> {data.lens}</Text>
+      <Text style={{fontSize: 16, marginLeft: 10,color: 'rgb(54,69,79)'}}><Text style={{fontWeight:'bold'}}>Haptic Material:</Text> {data.hapmaterial}</Text>
+      <Text style={{fontSize: 16, marginLeft: 10,color: 'rgb(54,69,79)'}}><Text style={{fontWeight:'bold'}}>Optic Material: </Text>{data.opmaterial}</Text>
+      <Text style={{fontSize: 16, marginLeft: 10,fontWeight:'bold',color: 'rgb(54,69,79)'}}>Dimensions:</Text>
+      <Text style={{fontSize: 16, marginLeft: 50,color: 'rgb(54,69,79)'}}>HAPTIC: {data.haptic} </Text>
+      <Text style={{fontSize: 16,marginLeft: 50,marginBottom: 10,color: 'rgb(54,69,79)'}}>OPTIC:  {data.opticdia} </Text>
+
     </View>
     <View style={{flex:0.5,marginLeft:20, marginTop: 5}}>
       
       
     </View>
-  <Title1 style={{marginLeft: 10,color: iOSColors.gray,marginBottom:-20}}>Lens Specification</Title1>
-    <SafeAreaView style={{flex: 0.8, backgroundColor:'white',marginTop: 30,marginLeft: 10}}>
+  <Title1 style={{marginLeft: 10,color: '#1C4A7A',marginBottom:-20,fontSize: 17, fontWeight: '600'}}>Lens Specification</Title1>
+    <SafeAreaView style={{flex: 0.8, backgroundColor:'white',marginTop: 30,marginLeft: 20}}>
     
    
       <View style={{ flex: 1, flexDirection:'row'}}>
@@ -114,15 +117,15 @@ return (
           height: 10,
           borderRadius: 5,
           marginHorizontal: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.92)'
+          backgroundColor: 'rgba(22,122,121,0.3)'
         }}
          onSnapToItem={(index) => setIndex(index)}
         inactiveDotOpacity={0.4}
         inactiveDotScale={0.6}
         tappableDots={true}
       />
-<Title1 style={{marginTop:10,marginLeft: 10,color: iOSColors.gray }}>Optimized IOL Constants</Title1>
-      <View style={{ flex: 0.8, flexDirection:'row', justifyContent: 'center', marginLeft: 10}}>
+<Title1 style={{marginTop:10,marginLeft: 10,color: '#1C4A7A',fontSize: 17, fontWeight: '600' }}>Optimized IOL Constants</Title1>
+      <View style={{ flex: 0.8, flexDirection:'row', justifyContent: 'center', marginLeft: 20}}>
         <Carousel
           layout="default"
           layoutCardOffset={7}
@@ -146,19 +149,21 @@ return (
           height: 10,
           borderRadius: 5,
           marginHorizontal: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.92)'
+          backgroundColor: 'rgba(22,122,121,0.3)'
         }}
         inactiveDotOpacity={0.4}
         inactiveDotScale={0.6}
         tappableDots={true}
       />
     
-    <View style={styles.container}>
+    <View style={{flex: 1,
+    justifyContent: 'center',
+    padding: 10, backgroundColor: 'rgba(99, 142, 187, 0.2)'}}>
     
-        <Card>
-        <Title1 style={{marginLeft:10, color: iOSColors.gray }}>Additional Information</Title1>
-          <Text onPress={() => Linking.openURL(`${data.link}`)}style={{fontSize: 17, marginTop: 10, marginLeft:20, marginBottom: 10,fontWeight: 'bold'}}>Lens Product Information</Text>
-        </Card>
+    
+        <Title1 style={{marginLeft:10, color: '#1C4A7A',fontSize: 17, fontWeight: '600' }}>Additional Information</Title1>
+          <Text onPress={() => Linking.openURL(`${data.link}`)}style={{fontSize: 15, marginTop: 10, marginLeft:20, marginBottom: 10,fontWeight: 'bold', color: 'rgb(54,69,79)'}}>Lens Product Information</Text>
+
     </View>
   </View>
   </ScrollView>
